3b8da66065a5f4f0b5de408315dd4a6f102ab99c
Joshua Wilensky <joshwilensky@gmail.com>
2021-01-25 11:04:14 -0500

    Added Sum Array Algo

---
+++ 06-Sum-Array/sum-array.js
@@ -0,0 +1,10 @@
+var sumArray = function(arr) {
+  var result = 0;
+
+  for (var i = 0; i < arr.length; i++) {
+    var currentNumber = arr[i];
+    result += currentNumber;
+  }
+
+  return result;
+};
